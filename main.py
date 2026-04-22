9448cbaf6779113f08cbd247f28580399634168d
ruhan-2908 <ruhankb29@gmail.com>
2026-04-22 22:16:50 +0530

    Implement Templates and HTML files

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
-from flask import Flask, request
-app = Flask(__name__)
+from flask import Flask, request, render_template
+app = Flask(__name__,template_folder='templates')
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "<h2>Hello World</h2>"
-
-@app.route("/hello")
-def hello():
-    return "<h4>Hello World</h4>"
-
-@app.route("/greet/<name>")
-def greet(name):
-    return f"Hello {name}!"
+    myvalue = 'Ruhan'
+    myresult = 10+20
+    mylist = [10,20,30,40,50]
+    sample = 20
+    return render_template('index.html',myvalue=myvalue,myresult = myresult,mylist=mylist, mysample = sample)
 
 
-@app.route("/add/<int:number1>/<int:number2>")
-def add(number1,number2):
-    return f"{number1} + {number2} = {number1+number2}"
+@app.template_filter('reverse')
+def reverse_string(str):
+    return str[::-1]
 
-@app.route("/handle_url_params")
-def handle_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        name = request.args.get('name')
-        greeting = request.args.get('greeting')
-        return f"{greeting} {name}"
-    else:
-        return "Some arguments are missing"
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0',port = 5555, debug = True)
+    app.run(host = '0.0.0.0',debug = True)
