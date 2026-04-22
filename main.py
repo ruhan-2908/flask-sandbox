093ca9133ec9fe73092ee41a244dd2c9fb58f4af
ruhan-2908 <ruhankb29@gmail.com>
2026-04-22 23:12:20 +0530

    Handle form data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask ,render_template, request
-
+import pandas as pd
 app = Flask(__name__,template_folder='templates')
 
 @app.route('/',methods=['GET','POST'])
@@ -17,7 +17,17 @@ def index():
                 return "Failure from bad credentials"
         else:
             return "Failure from no input"
+        
 
+@app.route('/file_upload',methods=['POST'])
+def file_upload():
+    file = request.files['file']
+
+    if file.content_type == 'text/plain':
+        return file.read().decode()
+    elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
+        df = pd.read_excel(file)
+        return df.to_html()
 
 
 if __name__ == '__main__':
