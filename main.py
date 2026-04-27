0a36ba4674676c20a3b38554dde7b1606eae48b9
ruhan-2908 <ruhankb29@gmail.com>
2026-04-27 20:54:00 +0530

    Implement toCSV endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template, request
+from flask import Flask ,render_template, request, Response
 import pandas as pd
 app = Flask(__name__,template_folder='templates')
 
@@ -29,6 +29,20 @@ def file_upload():
         df = pd.read_excel(file)
         return df.to_html()
 
+
+@app.route("/convert_csv",methods=['POST'])
+def convert_csv():
+    file = request.files['file']
+    df = pd.read_excel(file)
+
+    response = Response(
+        df.to_csv(),
+        mimetype='text/csv',
+        headers={
+            'Content_Disposition': 'attachment; filename=result.csv'
+        }
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug = True)
