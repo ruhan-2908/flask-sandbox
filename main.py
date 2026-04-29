561d5d21fb4bcf7fa0ab85065efd51cce0518a41
ruhan-2908 <ruhankb29@gmail.com>
2026-04-29 19:52:08 +0530

    Implement Cookies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, make_response,request
 
 
 app = Flask(__name__,template_folder='templates')
@@ -8,6 +8,7 @@ app.secret_key = "SECRETTT"
 @app.route("/")
 def index():
     return render_template('index.html',message='Index')
+
 
 @app.route("/set_data")
 def set_data():
@@ -25,9 +26,33 @@ def get_data():
     else:
         return render_template('index.html',message=f'No session found.')
     
+
 @app.route('/clear_session')
 def clear_session():
     session.clear()
     return render_template('index.html',message='Session cleared')
+
+
+@app.route('/set_cookie')
+def set_cookie():
+    response = make_response(render_template('index.html',message='Cookie set.'))
+    response.set_cookie('cookie_name','cookie_value')
+    return response
+
+
+@app.route('/get_cookie')
+def get_cookie():
+    cookie_value = request.cookies['cookie_name']
+    return 
+
+
+@app.route("/remove_cookie")
+def remove_cookie():
+    response = make_response(render_template('index.html',message='Cookie removed.'))
+    response.set('cookie_name',expires=0)
+    return response
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
