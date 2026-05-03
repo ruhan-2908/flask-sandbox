a438403f80c1ac9eb0fe5ca7ae0522f7664c388b
ruhan-2908 <ruhankb29@gmail.com>
2026-05-03 16:23:44 +0530

    Implementation of blueprints

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from app import create_app
+from blueprintapp.app import create_app
 
 flask_app = create_app()
 
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0',debug = True)
+    flask_app.run(host='0.0.0.0',debug=True)
