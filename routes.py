179b467ceb6aec0bc01e761ef697c6fa01955ddc
ruhan-2908 <ruhankb29@gmail.com>
2026-05-03 12:12:30 +0530

    Implement User Authentication

--- routes.py
+++ routes.py
@@ -1,35 +1,52 @@
-from flask import render_template, request
-from models import Person
+from flask import render_template, request, redirect,url_for
+from models import User
+from flask_login import login_user, logout_user, current_user, login_required
 
-def register_routes(app,db):
+
+def register_routes(app,db,bcrypt):
 
     @app.route("/",methods=['GET','POST'])
     def index():
+        return render_template('index.html')
+    
+
+    @app.route('/signup',methods=['GET','POST'])
+    def signup():
         if request.method == 'GET':
-            people = Person.query.all()
-            return render_template('index.html',people=people)
+            return render_template('signup.html')
         elif request.method == 'POST':
-            name = request.form.get('name')
-            age = request.form.get('age')
-            job = request.form.get('job')
+            username = request.form.get('username')
+            password = request.form.get('password')
 
-            person = Person(name=name,age=age,job=job)
+            hashed_password = bcrypt.generate_password_hash(password)
 
-            db.session.add(person)
+            user = User(username=username,password = hashed_password)
+
+            db.session.add(user)
             db.session.commit()
 
-            people = Person.query.all()
-            return render_template('index.html',people=people)
-    
-    @app.route('/delete/<pid>',methods=['DELETE'])
-    def delete(pid):
-        Person.query.filter(Person.pid == pid).delete()
-        db.session.commit()
+            return redirect(url_for('index'))
 
-        people = Person.query.all()
-        return render_template('index.html',people=people)
     
-    @app.route('/details/<pid>')
-    def details(pid):
-        person = Person.query.filter(Person.pid == pid).first()
-        return render_template('details.html',person=person)
+    @app.route('/login',methods=['GET','POST'])
+    def login():
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+
+            user = User.query.filter(User.username == username).first()
+
+            if bcrypt.check_password_hash(user.password,password):
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                return 'Failed'
+
+
+    @app.route("/logout")
+    def logout():
+        logout_user()
+        return redirect(url_for('index'))
